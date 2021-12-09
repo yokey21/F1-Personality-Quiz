f4a368ca08fc8b41d9c6f0b6c78f2c511c008587
Adam Byron <abyron939@gmail.com>
2021-12-09 14:22:26 -0800

    Initial Commit

---
+++ F1 Personality Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  F1 Personality Quiz
+//
+//  Created by Adam Byron on 12/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ F1 Personality QuizUITests/F1_Personality_QuizUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  F1_Personality_QuizUITestsLaunchTests.swift
+//  F1 Personality QuizUITests
+//
+//  Created by Adam Byron on 12/9/21.
+//
+
+import XCTest
+
+class F1_Personality_QuizUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
