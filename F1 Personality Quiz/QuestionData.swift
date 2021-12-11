b80f5a94fc2a516c666328ff0c77e70a0e8ddaf0
Adam Byron <abyron939@gmail.com>
2021-12-11 14:48:03 -0800

    New Questions Types

---
+++ F1 Personality Quiz/QuestionData.swift
@@ -0,0 +1,47 @@
+//
+//  QuestionData.swift
+//  F1 Personality Quiz
+//
+//  Created by Adam Byron on 12/11/21.
+//
+
+import Foundation
+
+struct Question {
+    var text: String
+    var type: ResponseType
+    var answers: [Answer]
+}
+
+enum ResponseType {
+    case single, multiple, ranged
+}
+
+//text - description
+struct Answer {
+    var text: String
+    var driver: Driver
+}
+
+
+enum Driver: Character {
+    case dog = "🐶", cat = "🐱", rabbit = "🐰", turtle = "🐢"
+    
+    var definition: String {
+        switch self {
+        case .dog:
+            return "You are incredibly outgoing. You surround yourself with the people you love and enjoy activities with your friends."
+        case .cat:
+            return "Mischievous, yet mild-tempered, you enjoy doing things on your own terms."
+        case .rabbit:
+            return "You love everything that's soft. You are healthy and full of energy."
+        case .turtle:
+            return "You are wise beyond your years, and you focus on the details. Slow and steady wins the race."
+            
+        }
+        
+    }
+}
+
+
+
--- F1 Personality Quiz/QuestionViewController.swift
+++ F1 Personality Quiz/QuestionViewController.swift
@@ -9,6 +9,40 @@ import UIKit
 
 class QuestionViewController: UIViewController {
 
+    var questionIndex = 0
+    
+    var questions: [Question] = [
+        Question(text: "Which food do you like the most?",
+                 type:.single,
+                 answers: [
+                    Answer(text: "Steak", driver: .dog),
+                    Answer(text: "Fish", driver: .cat),
+                    Answer(text: "Carrots", driver: .rabbit),
+                    Answer(text: "Corn", driver: .turtle)
+                          ]),
+        
+        Question(text: "Which activities do you enjoy?",
+                 type: .multiple,
+                 answers: [
+                    Answer(text: "Swimming", driver: .turtle),
+                    Answer(text: "Sleeping", driver: .cat),
+                    Answer(text: "Cuddling", driver: .rabbit),
+                    Answer(text: "Eating", driver: .dog)
+                          ]),
+        
+        Question(text: "How much do you enjoy car rides?",
+                 type: .ranged,
+                 answers: [
+                    Answer(text: "I dislike them", driver: .cat),
+                    Answer(text: "I get a little nervous",
+                           driver: .rabbit),
+                    Answer(text: "I barely notice them",
+                           driver: .turtle),
+                    Answer(text: "I love them", driver: .dog)
+        ])
+    ]
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
