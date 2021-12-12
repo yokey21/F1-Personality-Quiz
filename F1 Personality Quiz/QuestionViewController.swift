c870c54d296972186ab8e87c59c8d4316cefdd95
Adam Byron <abyron939@gmail.com>
2021-12-11 17:40:39 -0800

    Coded Outlets and Actions

--- F1 Personality Quiz/QuestionViewController.swift
+++ F1 Personality Quiz/QuestionViewController.swift
@@ -7,9 +7,81 @@
 
 import UIKit
 
+var questionIndex = 0
+var answersChosen: [Answer] = []
+
 class QuestionViewController: UIViewController {
 
-    var questionIndex = 0
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet weak var singleButton1: UIButton!
+    @IBOutlet weak var singleButton2: UIButton!
+    @IBOutlet weak var singleButton3: UIButton!
+    @IBOutlet weak var singleButton4: UIButton!
+    
+    @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet weak var multipleButton1: UIButton!
+    @IBOutlet weak var multipleButton2: UIButton!
+    @IBOutlet weak var multipleButton3: UIButton!
+    @IBOutlet weak var multipleButton4: UIButton!
+    
+    @IBOutlet weak var multiSwitch1: UISwitch!
+    @IBOutlet weak var multiSwitch2: UISwitch!
+    @IBOutlet weak var multiSwitch3: UISwitch!
+    @IBOutlet weak var multiSwitch4: UISwitch!
+    
+    
+    @IBOutlet weak var rangedStackView: UIStackView!
+    @IBOutlet weak var rangedLabel1: UILabel!
+    @IBOutlet weak var rangedLabel2: UILabel!
+    @IBOutlet weak var rangedSlider: UISlider!
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        let currentAnswers = questions[questionIndex].answers
+        switch sender {
+            case singleButton1:
+                answersChosen.append(currentAnswers[0])
+            case singleButton2:
+                answersChosen.append(currentAnswers[1])
+            case singleButton3:
+                answersChosen.append(currentAnswers[2])
+            case singleButton4:
+                answersChosen.append(currentAnswers[3])
+            default:
+                break
+        }
+        nextQuestion()
+    }
+    
+    @IBAction func multipleAnswerButtonPressed() {
+        let currentAnswers = questions[questionIndex].answers
+        
+            if multiSwitch1.isOn {
+                answersChosen.append(currentAnswers[0])
+            }
+            if multiSwitch2.isOn {
+                answersChosen.append(currentAnswers[1])
+            }
+            if multiSwitch3.isOn {
+                answersChosen.append(currentAnswers[2])
+            }
+            if multiSwitch4.isOn {
+                answersChosen.append(currentAnswers[3])
+            }
+        
+            nextQuestion()
+    }
+    
+    @IBAction func rangedAnswerButtonPressed(_ sender: Any) {
+        let currentAnswers = questions[questionIndex].answers
+        let index = Int(round(rangedSlider.value * Float(currentAnswers.count - 1)))
+        answersChosen.append(currentAnswers[index])
+        nextQuestion()
+    }
     
     var questions: [Question] = [
         Question(text: "Which food do you like the most?",
@@ -45,11 +117,70 @@ class QuestionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateUI()
     }
     
-
+    func updateUI() {
+        singleStackView.isHidden = true
+        multipleStackView.isHidden = true
+        rangedStackView.isHidden = true
+        
+        let currentQuestion = questions[questionIndex]
+        let currentAnswers = currentQuestion.answers
+        let totalProgress = Float(questionIndex)/Float(questions.count)
+        
+        navigationItem.title = "Question #\(questionIndex+1)"
+        questionLabel.text = currentQuestion.text
+        progressView.setProgress(totalProgress, animated: true)
+        
+        switch currentQuestion.type {
+            case .single:
+                singleStackView.isHidden = false
+            case .multiple:
+                multipleStackView.isHidden = false
+            case .ranged:
+                rangedStackView.isHidden = false
+        }
+    }
+    
+    func updateSingleStack(using answers: [Answer]) {
+        singleStackView.isHidden = false
+        singleButton1.setTitle(answers[0].text, for: .normal)
+        singleButton2.setTitle(answers[1].text, for: .normal)
+        singleButton3.setTitle(answers[2].text, for: .normal)
+        singleButton4.setTitle(answers[3].text, for: .normal)
+    }
+    
+    func updateMultipleStack(using answers: [Answer]) {
+        multipleStackView.isHidden = false
+        multiSwitch1.isOn = false
+        multiSwitch2.isOn = false
+        multiSwitch3.isOn = false
+        multiSwitch4.isOn = false
+        multipleButton1.setTitle(answers[0].text, for: .normal)
+        multipleButton2.setTitle(answers[0].text, for: .normal)
+        multipleButton3.setTitle(answers[0].text, for: .normal)
+        multipleButton4.setTitle(answers[0].text, for: .normal)
+        
+    }
+    
+    func updateRangedStack(using answers: [Answer]) {
+        rangedStackView.isHidden = false
+        rangedSlider.setValue(0.5, animated: false)
+        rangedLabel1.text = answers.first?.text
+        rangedLabel2.text = answers.last?.text
+    }
+    
+    
+    func nextQuestion() {
+        questionIndex += 1
+        
+        if questionIndex < questions.count {
+            updateUI()
+        } else {
+            performSegue(withIdentifier: "ResultsSegue", sender: nil)
+        }
+    }
     /*
     // MARK: - Navigation
 
--- F1 Personality Quiz/ResultsViewController.swift
+++ F1 Personality Quiz/ResultsViewController.swift
@@ -8,11 +8,21 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
+    
+    var responses: [Answer]!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ResultsSegue" {
+            let resultsViewController = segue.destination as! ResultsViewController
+            resultsViewController.responses = answersChosen
+        }
     }
     
 
